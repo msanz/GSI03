c49a5fffd03fe5bdaf84554c73ff2f932a880add
Usuario <Usuario@Usuario-PC>
2015-09-29 01:58:22 +0200

    Añadida documentacion a las clases EventSystem, Client, Concert, CreditCard y Location. En concert, añadido " ; " linea 157. Ponia que se esperaba.

--- GSIGR03/src/GSILabs/BModel/CreditCard.java
+++ GSIGR03/src/GSILabs/BModel/CreditCard.java
@@ -5,17 +5,25 @@
  */
 package GSILabs.BModel;
 
-/**
+/** This provides the credit card object. A simply string
  * 
  * @author GR03
  */
 class CreditCard {
     private String number;
     
+    /**
+     * initialice the creditcard number
+     * @param number the credit card number to be added
+     */
     CreditCard(String number){
         this.number = number;
     }
     
+    /**
+     * Returns the string value of this credit card
+     * @return a string with the number
+     */
     public String getCreditCard(){
         return this.number;
     }
