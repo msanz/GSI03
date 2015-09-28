6776fa2630afdcd9a2b7857871aa9e25b0ce6fa4
Usuario <Usuario@Usuario-PC>
2015-09-29 01:51:24 +0200

    Añadida documentacion

--- GSIGR03/src/GSILabs/BModel/Ticket.java
+++ GSIGR03/src/GSILabs/BModel/Ticket.java
@@ -9,7 +9,8 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- *
+ * A ticket is a object that allows to enter to a event. Is associated with event name.
+ * All the tickets are diferent
  * @author GR03
  */
 public class Ticket {
@@ -21,21 +22,39 @@ public class Ticket {
     float price;
     boolean used;
     
+    /**
+     * ticket constructor. Only needs a event name, and a atomic integer object initializated.
+     * @param eventName The name of the event
+     * @param atomicInteger The atomic integer object, for creating ticket's ids
+     */
     public Ticket (String eventName, AtomicInteger atomicInteger){
         this.eventName = eventName;
         this.id = (int)atomicInteger.getAndIncrement();
         this.used = false;
     }
     
+    /**
+     * Set the number of persons that this ticket is going to authorice 
+     * @param numberOfPersons number of persons for this ticket
+     * 
+     */
     public void setNumberOfPersons(int numberOfPersons){
         this.numberOfPersons = numberOfPersons;
         this.numberOfUsed = numberOfPersons;
     }
     
+    /**
+     * A simply method for check ticket status
+     * @return used var, that can be true if used or false if not
+     */
     public boolean checkTicketUsed(){
         return used;
     }
     
+    /**
+     * This method checks how many times a ticket was used, and mark it as used if number of used
+     * is 0. If not 0, decrement it one unit
+     */
     public void useTicket() {
         
         if (checkTicketUsed())
@@ -45,15 +64,26 @@ public class Ticket {
                 this.setUsed();
         }
     }
-   
+    
+    /**
+     * This method only set used to true
+     */
     private void setUsed(){
         this.used = true;
     }
     
+    /**
+     * Sets a ticket's price passed in float parameter
+     * @param price the price of the ticket
+     */
     public void setPrice(float price){
         this.price = price;
     }
     
+    /**
+     * This only returns the price attribute
+     * @return the price of this ticket 
+     */
     public float getPrice()
     {
         return price;
@@ -84,11 +114,13 @@ public class Ticket {
                 " numberOfUsed: " + numberOfUsed;
     }
     
+    /**
+     * This returns a string with the name of the ticket's event
+     * @return 
+     */
     public String getEventNameInTicket() {
         return this.eventName;
     }
     
-    public boolean getUsedStatus() {
-        return used;
-    }
+    
 }
