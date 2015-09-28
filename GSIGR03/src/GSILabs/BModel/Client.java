900bfa46e1e1c5cd82d7b7c043802e02b0ce28e2
Daniel Díez <danihzt@gmail.com>
2015-09-28 19:33:54 +0200

    Update Client.java

--- GSIGR03/src/GSILabs/BModel/Client.java
+++ GSIGR03/src/GSILabs/BModel/Client.java
@@ -1,15 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This document is part of the lab material for the subject:
+ * Gestion de Sistemas de Informacion
+ * to be released at the
+ * Universidad Publica de Navarra
+ * during the first semester of the Academic Year 2015-2016
  */
 package GSILabs.BModel;
 
 import java.util.HashSet;
 
 /**
- *
+ * A client is someone who buy a ticket
  * @author GR03
+ * @version 1.0
  */
 public class Client {
     
@@ -21,6 +24,14 @@ public class Client {
     private HashSet<String> creditCard;
     private HashSet<Ticket> sales;
     
+     /**
+     * Create a new client
+     * @param DNI A number that indentify the client
+     * @param name Name of client
+     * @param surName1 First surname of client
+     * @param surName2 Second surname of client
+     * @param birthday The day and time that client born 
+     */
     public Client (int DNI, String name, String surName1, String surName2, DateGeneral birthday){
         this.DNI = DNI;
         this.name = name;
@@ -32,7 +43,7 @@ public class Client {
     }
     
     /** 
-     * 
+     * Validate if a credit card is well formed
      * @param card string with credit card number
      * @return true if the string is formed by 16 numeric digits
      */
@@ -40,6 +51,11 @@ public class Client {
         return (card.matches("\\d{16,16}") && !card.equals(""));
     }
     
+    /**
+     * Add a credit card to the credit pool
+     * @param card string with credit card
+     * @return true if the credit card is well formed and it adds to the pool false in other cas
+     */
     public boolean addCreditCard(String cCard){
         if(validateCreditCard(cCard))
         {
@@ -48,30 +64,59 @@ public class Client {
         return false;
     }
     
+     /**
+     * Add ticket to the tickets pool
+     * @param t Ticket object
+     * @return truei if it adds to the pool false in other case
+     */
     public boolean addTicket(Ticket t) {
         return this.sales.add(t);
     }
     
+    /**
+     * Returns the DNI associated with the client
+     * @return The DNI of the client.
+     */
     public int getDNI() {
         return DNI;
     }
-
+    
+    /**
+     * Returns the name associated with the client
+     * @return The name of the client.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the first surname associated with the client
+     * @return The first surname of the client.
+     */
     public String getSurName1() {
         return surName1;
     }
 
+    /**
+     * Returns the second surname associated with the client
+     * @return The second surname of the client.
+     */
     public String getSurName2() {
         return surName2;
     }
-
+    
+    /**
+     * Returns the birthday associated with the client
+     * @return The birthday of the client
+     */
     public DateGeneral getBirthday() {
         return birthday;
     }
 
+    /**
+     * Returns the credits card pool associated with the client
+     * @return The credits card pool of the client
+     */
     public HashSet<String> getCreditCard() {
         return creditCard;
     }
@@ -100,6 +145,10 @@ public class Client {
         return hash;
     }
     
+     /**
+     * Rewriting the toString() method
+     * @return Other style to show the information
+     */
     @Override
     public String toString(){
         return "DNI: " + getDNI() +
