067dfce43974550e6886999d82befcc1f542e2ff
txingu <iechavarribeloqui@gmail.com>
2015-09-30 00:42:57 +0200

    Modified DateComparator to compare Dates as Long integer.

--- GSIGR03/src/GSILabs/BModel/DateComparator.java
+++ GSIGR03/src/GSILabs/BModel/DateComparator.java
@@ -9,6 +9,7 @@
 package GSILabs.BModel;
 
 import java.util.Comparator;
+import java.util.Date;
 
 /**
  * Implements Comparator in order to can sort the concerts
@@ -18,6 +19,22 @@ import java.util.Comparator;
 public class DateComparator implements Comparator<DateEvent> {
     @Override
     public int compare(DateEvent o1, DateEvent o2) {
+        Date normalDateStart1 = o1.getDayStart();
+        Date normalDateStart2 = o2.getDayStart();
+        Date normalDateFinish1 = o1.getDayFinish();
+        Date normalDateFinish2 = o2.getDayFinish();
+        
+        long date1StartInLong = normalDateStart1.getTime();
+        long date2StartInLong = normalDateStart2.getTime();
+        long date1FinishInLong = normalDateFinish1.getTime();
+        long date2FinishInLong = normalDateFinish2.getTime();
+        
+        if (date1StartInLong == date2StartInLong) {
+            return 1;
+        }
+        else
+            return 0;
+        /*
         if (o1.getDayStart().before(o2.getDayStart())){
             return -1;
         }
@@ -25,8 +42,9 @@ public class DateComparator implements Comparator<DateEvent> {
         if (o1.getDayStart().after(o2.getDayStart())){
             return 1;
         }
+        */
         
-        return 0;
+        //return 0;
     }
     
 }
