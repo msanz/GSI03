99db6fce6bb49f9c47b4bc5c19ac75190a93a1da
Daniel <danihzt@gmail.com>
2015-10-08 12:24:37 +0200

    Comenzamos la practica 2

---
+++ GSIGR03/src/GSILabs/Misc/SSTest01.java
@@ -0,0 +1,19 @@
+/*
+ * This document is part of the lab material for the subject:
+ * Gestion de Sistemas de Informacion
+ * to be released at the
+ * Universidad Publica de Navarra
+ * during the first semester of the Academic Year 2015-2016
+ */
+
+package GSILabs.Misc;
+
+/**
+ * Ejercicio 2
+ * @author GR03
+ */
+public class SSTest01 {
+    public static void main(String args[]){  
+        System.out.println("SSTest01");
+    }
+}
