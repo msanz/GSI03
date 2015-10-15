eec5ee3c43912cf4ba7df48bdd80c2b9a06ffee1
linux1 <linux1@localhost>
2015-10-15 13:39:35 +0200

    Ejer SSTest02 casi terminado (retraso maximo con los for)

--- GSIGR03/src/GSILabs/Misc/SSTest02.java
+++ GSIGR03/src/GSILabs/Misc/SSTest02.java
@@ -8,11 +8,15 @@
 
 package GSILabs.Misc;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import org.jopendocument.dom.OOUtils;
+import org.jopendocument.dom.spreadsheet.Sheet;
 import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
 
@@ -23,20 +27,42 @@ import org.jopendocument.dom.spreadsheet.SpreadSheet;
 public class SSTest02 {
     public static void main(String args[]) throws IOException{ 
         // Create the data to save.
-        final Object[][] data = new Object[4][2];
-        data[0] = new Object[] { 1, 3 };
-        data[1] = new Object[] { 2, 10 };
-        data[2] = new Object[] { 4, 15 };
-        data[3] = new Object[] { 5, 18 };
 
-        String[] columns = new String[] { "Month", "Temp" };
+        
+        //defined matrix
+        try {
+            final int [][] data = {{7, 4, 7, 5, 19, 5,},{4, 7, 6, 18, 1, 6},{3, 11, 6, 15, 8, 9},{9, 8, 7, 22, 7, 13}}; 
+            //Define new table
+            final File file = new File("test02.ods");
+            DefaultTableModel model = new DefaultTableModel(7,11);
+            SpreadSheet.createEmpty(model).saveAs(file);
+            Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
+            int k=0;
+            for (int i = 4; i < 7; i++) {
+                for (int j = 2; j < 11; j++) {
+                    if (data[i-4][k] >= 10){
+                        sheet.getCellAt(j,i).setValue(data[i-4][k]);
+                        sheet.getCellAt(j,i).setBackgroundColor(Color.blue);
+                    }
+                    else {
+                        System.out.println((i-4) + " " + (k));
+                        sheet.getCellAt(j,i).setValue(data[i-4][k]);
+                        sheet.getCellAt(j,i).setBackgroundColor(Color.red);
 
-        TableModel model = new DefaultTableModel(data, columns);  
+                    }
 
-        // Save the data to an ODS file and open it.
-        final File file = new File("temperature.ods");
-        SpreadSheet.createEmpty(model).saveAs(file);
-
-        OOUtils.open(file);
+                }
+            }
+            for (int i = 0; i < 11; i++) {
+                sheet.getCellAt(i,0).setValue("");
+                
+            }
+            OOUtils.open(sheet.getSpreadSheet().saveAs(file));
+            }
+        catch (IOException ex){
+            Logger.getLogger(SSTest02.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
     }
 }
+
