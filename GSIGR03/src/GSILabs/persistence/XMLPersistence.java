a39feaf9020fa6d62398ad5ae04587a359a4cf44
Maite <msanzochoa@gmail.com>
2015-11-05 13:32:56 +0100

    modificado: GSIGR03/nbproject/private/private.xml 	modificado: GSIGR03/src/GSILabs/persistence/XMLPersistence.java

--- GSIGR03/src/GSILabs/persistence/XMLPersistence.java
+++ GSIGR03/src/GSILabs/persistence/XMLPersistence.java
@@ -7,11 +7,129 @@
  */
 package GSILabs.persistence;
 
+import GSILabs.BModel.Artist;
+import GSILabs.BModel.Client;
+import GSILabs.BModel.Collective;
+import GSILabs.BModel.Concert;
+import java.io.File;
+
 /**
  * Read a file XML
  * @author GR03
  * @version 1.0
  */
 public class XMLPersistence {
+   
+    public static Artist parseArtist(String str) {
+        return null;
+    }
+    
+    public static Artist parseArtist(File f) {
+        return null;
+    }
+    
+    public static Client parseClient(String str) {
+        return null;
+    }
+    
+    public static Client parseClient(File f) {
+        return null;
+    }
+    
+    public static Collective parseCollective(String str) {
+        return null;
+    }
+    
+    public static Collective parseCollective(File f) {
+        return null;
+    }
+    
+    public static Concert parseArtist(String str) {
+        return null;
+    }
+    
+    public static Concert parseArtist(File f) {
+        return null;
+    }
+    
+    public static Coordinates parseArtist(String str) {
+        return null;
+    }
+    
+    public static Coordinates parseArtist(File f) {
+        return null;
+    }
+    
+    public static DateEvent parseArtist(String str) {
+        return null;
+    }
+    
+    public static DateEvent parseArtist(File f) {
+        return null;
+    }
+    
+    public static DateGeneral parseArtist(String str) {
+        return null;
+    }
+    
+    public static DateGeneral parseArtist(File f) {
+        return null;
+    }
+    
+    public static Event parseArtist(String str) {
+        return null;
+    }
+    
+    public static Event parseArtist(File f) {
+        return null;
+    }
+    
+    public static Exhibition parseArtist(String str) {
+        return null;
+    }
+    
+    public static Exhibition parseArtist(File f) {
+        return null;
+    }
+    
+    public static Festival parseArtist(String str) {
+        return null;
+    }
+    
+    public static Festival parseArtist(File f) {
+        return null;
+    }
+    
+    public static Location parseArtist(String str) {
+        return null;
+    }
+    
+    public static Location parseArtist(File f) {
+        return null;
+    }
+    
+    public static Performer parseArtist(String str) {
+        return null;
+    }
+    
+    public static Performer parseArtist(File f) {
+        return null;
+    }
+    
+    public static Sale parseArtist(String str) {
+        return null;
+    }
+    
+    public static Sale parseArtist(File f) {
+        return null;
+    }
+    
+    public static Ticket parseTicket(String str) {
+        return null;
+    }
+    
+    public static Ticket parseTicket(File f) {
+        return null;
+    }
     
 }
